0efb224c1106b0e65f0690a6532aa7c61b68f7dc
Keith Bingman <kb@wantering.com>
2014-06-11 15:45:47 -0700

    requiring lodash

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ module.exports = function(fileName, options) {
         }
         // Unwrapped
         buffer.unshift("var templates = {};");
+        buffer.unshift("};");
+        buffer.unshift("_ = require('lodash-node');");
+        buffer.unshift("if (typeof require !== 'undefined' && typeof _ !== 'function') {");
 
         // All wrappers return the templates object
         if (options.wrapper) {
